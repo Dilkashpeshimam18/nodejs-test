de5f2232fb40635c281e228c6b211085393a6553
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-03-22 02:18:37 -0700

    displaying orders on screen as per table

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ addOrder.addEventListener('click', async (e) => {
         try {
             const res = await axios.post('http://localhost:4000/add-order', ordersObj)
             console.log(res)
+            displayOrder(res.data.data)
+            dishPrice.value = ''
+            dishName.value = ''
+            table.value = ''
         } catch (err) {
             console.log(err)
         }
@@ -34,8 +38,29 @@ addOrder.addEventListener('click', async (e) => {
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         const orders = await axios.get('http://localhost:4000/get-orders')
-        console.log(orders)
+        const result = orders.data.allOrders
+        result.forEach((order) => {
+            displayOrder(order)
+        })
     } catch (err) {
         console.log(err)
     }
 })
+
+
+function displayOrder(ordersObj) {
+    if (ordersObj.table == 'Table1') {
+        let tableList = document.querySelector('#list1')
+        let tableTag = `<li id='${ordersObj.id}'> ${ordersObj.dish} ${ordersObj.price}  <button onClick=deleteOrder('${ordersObj.id}')>Delete</button></li> `
+        tableList.innerHTML = tableList.innerHTML + tableTag
+    } else if (ordersObj.table == 'Table2') {
+        let tableList = document.querySelector('#list2')
+        let tableTag = `<li id='${ordersObj.id}'> ${ordersObj.dish} ${ordersObj.price}  <button onClick=deleteOrder('${ordersObj.id}')>Delete</button></li> `
+        tableList.innerHTML = tableList.innerHTML + tableTag
+    } else {
+        let tableList = document.querySelector('#list3')
+        let tableTag = `<li id='${ordersObj.id}'> ${ordersObj.dish} ${ordersObj.price}  <button onClick=deleteOrder('${ordersObj.id}')>Delete</button></li> `
+        tableList.innerHTML = tableList.innerHTML + tableTag
+    }
+
+}
